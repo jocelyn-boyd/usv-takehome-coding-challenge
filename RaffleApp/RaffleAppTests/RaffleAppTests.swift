ff3200757d73672524725a7ecb8c147303de3b97
Jocelyn B <jocelynboyd@pursuit.org>
2021-05-28 16:50:53 -0400

    create project file

--- RaffleApp/RaffleAppTests/RaffleAppTests.swift
+++ RaffleApp/RaffleAppTests/RaffleAppTests.swift
@@ -6,7 +6,6 @@
 //
 
 import XCTest
-@testable import RaffleApp
 
 class RaffleAppTests: XCTestCase {
 
@@ -25,7 +24,7 @@ class RaffleAppTests: XCTestCase {
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
-        self.measure {
+        measure {
             // Put the code you want to measure the time of here.
         }
     }

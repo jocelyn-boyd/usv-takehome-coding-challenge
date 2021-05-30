68c6f19cea25242bd1509a964235049f7f847fb5
Jocelyn B <jocelynboyd@pursuit.org>
2021-05-29 22:00:55 -0400

    add raffle winner screeen

---
+++ RaffleApp/RaffleApp/Screens/View Controllers/RaffleWinnerViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RaffleWinnerViewController.swift
+//  RaffleApp
+//
+//  Created by Jocelyn Boyd on 5/29/21.
+//
+
+import UIKit
+
+class RaffleWinnerViewController: UIViewController {
+
+	@IBOutlet weak var participantNameLabel: UILabel!
+	@IBOutlet weak var dateRegisteredLabel: UILabel!
+	@IBOutlet weak var participantIdLabel: UILabel!
+	@IBOutlet weak var participantEmailLabel: UILabel!
+	@IBOutlet weak var participantPhoneNumberLabel: UILabel!
+	
+	override func viewDidLoad() {
+        super.viewDidLoad()
+	}
+}
